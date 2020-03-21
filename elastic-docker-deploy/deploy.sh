3a6c7acc1e6e308139c48a92032a60e5b5b7cc2c
Ahmed Hammami <ahmed.hammami@polymtl.ca>
2020-03-20 22:59:43 -0400

    updating deployment files

--- elastic-docker-deploy/deploy.sh
+++ elastic-docker-deploy/deploy.sh
@@ -1,3 +1,4 @@
+sed -i "s/{elasticsearch-binaries}/elasticsearch-7.6.1-linux-x86_64.tar.gz/g" Dockerfile
 docker build -t elastic-self-image .
 sed -i "s/{docker-image}/elastic-self-image/g" docker-compose.yml
 docker-compose up -d
