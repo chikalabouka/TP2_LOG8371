60fd70a7ca6b16b2bc0bae7e65a2c318abb8f290
Ahmed Hammami <ahmed.hammami@polymtl.ca>
2020-03-21 12:55:34 -0400

    fixing script

--- elastic-docker-deploy/deploy.sh
+++ elastic-docker-deploy/deploy.sh
@@ -1,5 +1,5 @@
 elasticbinaries=$(ls | grep *.tar.gz)
-echo $elasticbinaries
+echo binary file is : $elasticbinaries
 sed -i "s/{elasticsearch-binaries}/$elasticbinaries/g" Dockerfile
 docker build -t elastic-self-image .
 sed -i "s/{docker-image}/elastic-self-image/g" docker-compose.yml
